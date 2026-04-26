77c54f45adb85de1392b002349a5681e7e300a6d
trexel <nikhileshj23@iitk.ac.in>
2026-04-26 06:48:34 +0530

    k

--- code/kernel_files/proportion_policy/e1000_main.c
+++ code/kernel_files/proportion_policy/e1000_main.c
@@ -15,7 +15,7 @@
 
 DEFINE_PER_CPU(bool, in_clean_alloc);
 DEFINE_PER_CPU(bool, in_rx_alloc);
-DEFINE_PER_CPU(struct policy_counter, packet_counter);
+DEFINE_PER_CPU(struct policy_counters, packet_counter);
 
 EXPORT_PER_CPU_SYMBOL(in_rx_alloc);
 EXPORT_PER_CPU_SYMBOL(in_clean_alloc);
